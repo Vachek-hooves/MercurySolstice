cd91b94cf640d8fd78cd7cf6cccb47d8a75e6a5c
Vachek-hooves <vachek0602@gmail.com>
2025-01-20 11:04:44 +0200

    updates

--- NavMenu/TabNav.js
+++ NavMenu/TabNav.js
@@ -6,11 +6,11 @@ const Tab = createBottomTabNavigator();
 const TabNav = () => {
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Advice" component={Advice} />
+      <Tab.Screen name="Timer" component={Timer} />
       <Tab.Screen name="Diary" component={Diary} />
+      <Tab.Screen name="Advice" component={Advice} />
       <Tab.Screen name="Game" component={Game} />
       <Tab.Screen name="Settings" component={Settings} />
-      <Tab.Screen name="Timer" component={Timer} />
     </Tab.Navigator>
   );
 };
