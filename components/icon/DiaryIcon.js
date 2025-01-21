f75699e7515a0dcc16829fe4fae16b644b40b7df
Vachek-hooves <vachek0602@gmail.com>
2025-01-21 13:54:54 +0200

    Diary icon bouncing

--- components/icon/DiaryIcon.js
+++ components/icon/DiaryIcon.js
@@ -1,20 +1,78 @@
-import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {StyleSheet, View, Image, TouchableOpacity, Animated} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 const DiaryIcon = () => {
   const navigation = useNavigation();
+  const bounceAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    startBounceAnimation();
+  }, []);
+
+  const startBounceAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(bounceAnim, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(bounceAnim, {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  };
+
+  const bounceInterpolation = bounceAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, -10], // Adjust this value to control bounce height
+  });
+
   return (
-    <View style={{position: 'absolute', top: 70, right: 50}}>
+    <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.navigate('DiaryDetails')}>
-        <Image
-          source={require('../../assets/icons/diary.png')}
-          style={{width: 40, height: 40}}
-        />
+        <Animated.View
+          style={[
+            styles.iconContainer,
+            {
+              transform: [{translateY: bounceInterpolation}],
+            },
+          ]}>
+          <Image
+            source={require('../../assets/icons/diary.png')}
+            style={styles.icon}
+          />
+        </Animated.View>
       </TouchableOpacity>
     </View>
   );
 };
 
-export default DiaryIcon;
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 70,
+    right: 50,
+  },
+  iconContainer: {
+    // Add shadow or other styling here if needed
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default DiaryIcon;
