4059968f8243a27e4ee07c617b4ebb401937ad4d
Vachek-hooves <vachek0602@gmail.com>
2025-01-28 15:36:00 +0200

    update bg, icon

--- components/Layout/MainLayout.js
+++ components/Layout/MainLayout.js
@@ -1,17 +1,14 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 
 const MainLayout = ({children}) => {
   return (
-    <LinearGradient
-      colors={['#00336E', '#00336C', '#002E62', '#002957', '#00254F']}
-      style={styles.container}>
-      {/* <View style={styles.content}> */}
-
+    <ImageBackground
+      style={styles.container}
+      source={require('../../assets/bg/bg.jpg')}>
       {children}
-      {/* </View> */}
-    </LinearGradient>
+    </ImageBackground>
   );
 };
 
